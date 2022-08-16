648e93bad8d9fd3a3f7a4c48e7c7e593b49a6799
Alyssa Nicoll <alyssaamnell@gmail.com>
2022-08-16 18:52:19 -0500

    Initial commit?

---
+++ WRAPPERAPP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WRAPPERAPP;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ WRAPPERAPP/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WRAPPERAPP;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
